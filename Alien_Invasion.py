88b1876ebafb3e0bd7533d0f7650eb81f3c61df5
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2023-03-10 18:29:59 -0500

    added alien file

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from Alien import Alien
 
 class AlienInvasion:
     """Overall Class to manage game assets and behaviors"""
@@ -20,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -74,6 +78,24 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
             # print(len(self.bullets))
+    
+    def _create_fleet(self):
+        """create the fleet of aliens"""
+        #Create an alien and find the number of aliens in a row
+        #Spacing between each alien is equal to one alien width
+
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x // (2*alien_width)
+
+        #Create first row of aliens
+        for alien_number in range(number_aliens_x):
+            #create an alien and place it in a row
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x  = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Redraw the screen during each pass through the loop."""
@@ -81,7 +103,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-
+        self.aliens.draw(self.screen)
             # Make the most recently drawn screen visible.
         pygame.display.flip()
     
